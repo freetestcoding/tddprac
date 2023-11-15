3133a3c1c17ca5d09dde5ce4f499c41ba16482c1
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2023-11-15 10:28:01 +0900

    test case 추가

--- src/test/java/com/ohgiraffers/tdd_test/CalculatorTests.java
+++ src/test/java/com/ohgiraffers/tdd_test/CalculatorTests.java
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTests {
 
     @Test
-    public void sumTwoNumbers() {
+    public void sumTwoNumbers1() {
+        Calculator cal = new Calculator();
+        Assertions.assertEquals(3, cal.sumTwoNumbers(1, 2));
+    }
+
+    @Test
+    public void sumTwoNumbers2() {
         Calculator cal = new Calculator();
         Assertions.assertEquals(3, cal.sumTwoNumbers(1, 2));
     }
